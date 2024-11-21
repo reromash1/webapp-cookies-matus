91a42bdce7e957d8e5da82ab91b1f63618eea5ec
Elvis <davis-elvis@hotmail.com>
2024-11-21 09:05:51 -0500

    añadiendocookies

---
+++ src/main/java/controllers/LoginServlet.java
@@ -0,0 +1,75 @@
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Optional;
+
+//path o anotación
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    final static String USERNAME = "admin";
+    final static String PASSWORD = "12345";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies() != null ? req.getCookies() : new Cookie[0];
+        //busco en  el arreglo de cookie si existe la cookie
+        //solicitada y la convierto en String
+        Optional<String> cookieOptional = Arrays.stream(cookies)
+                .filter(c -> "username".equals(c.getName()))
+                //convertimos a string el valor encontrado
+                .map(Cookie::getValue)
+                .findAny();
+        if (cookieOptional.isPresent()) {
+            resp.setContentType("text/html;charset=UTF-8");
+            try(PrintWriter out = resp.getWriter()) {
+                //Creo la plantilla html
+                out.print("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<meta charset=\"utf-8\">");
+                out.println("<title>Bienvenido</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Hola "+cookieOptional.get()+" ya iniciaste sesión anteriormente!</h1>");
+                out.println("</body>");
+                out.println("</html>");
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+            //creamos la cookie
+            Cookie usernameCookie = new Cookie("username", username);
+            resp.addCookie(usernameCookie);
+            resp.setContentType("text/html;charset=UTF-8");
+            try (PrintWriter out = resp.getWriter()) {
+                //Creo la plantilla html
+                out.print("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<meta charset=\"utf-8\">");
+                out.println("<title>Bienvenido a la app</title>");
+                out.println("</head>");
+                out.println("<body>");
+                out.println("<h1>Bienvenido a mi APP</h1>");
+                out.println("</body>");
+                out.println("</html>");
+            }
+        } else {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Lo sentimos no tiene acceso");
+        }
+    }
+}
